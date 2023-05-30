182fd6bcc07593b01fa4345850db5ea1193a2f5e
Yasmine Gharbi <99125118+GHARBIyasmine@users.noreply.github.com>
2023-05-30 20:40:15 +0100

    Update database_connection.py

--- docker_image/database_connection.py
+++ docker_image/database_connection.py
@@ -6,8 +6,8 @@ import os
 host_server = os.environ.get('host_server', 'ppp-server.postgres.database.azure.com')
 db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
 database_name = os.environ.get('database_name', 'db_ppp')
-db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'pppadminserver')))
-db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'pilote@koussay12')))
+db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', '#')))
+db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', '#')))
 ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
 DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
 engine=create_engine(DATABASE_URL,
